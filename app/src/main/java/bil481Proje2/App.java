78e53948bb7a51ca5422ba779c35ce6d562ae3d3
Diyar <karadiyar66@gmail.com>
2021-09-25 14:03:41 +0300

    before denugging

--- app/src/main/java/bil481Proje2/App.java
+++ app/src/main/java/bil481Proje2/App.java
@@ -3,6 +3,19 @@
  */
 package bil481Proje2;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -10,17 +23,66 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        get("/", (req, res) -> "Hello World, sup kid?");
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map<String, String> map = new HashMap<String, String>();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1").replaceAll("\\s","");
+          int input1AsInt = Integer.parseInt(input1);
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
+
+          String input3 = req.queryParams("input3").replaceAll("\\s","");
+          int input3AsInt = Integer.parseInt(input3);
+
+          String input4 = req.queryParams("input4");
+          java.util.Scanner sc1 = new java.util.Scanner(input4);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          sc1.close();
+          System.out.println(inputList);
+
+
+          //boolean result = App.search(inputList, input2AsInt);
+          boolean result = App.isBigger(input1AsInt, input2AsInt, input3AsInt, inputList);
+
+          Map<String, Boolean> map = new HashMap<String, Boolean>();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+    
+
     }
 
-    public static boolean isBigger(int num1, int num2, int num3, int[] arr){
+    public static boolean isBigger(int num1, int num2, int num3, ArrayList<Integer> arr){
         // if array has num1, num2 and sum of these numbers are bigger than num3 return true, if not false
         boolean num1v = false;
         boolean num2v = false;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == num1){
+        for(int i=0; i<arr.size(); i++){
+            if(arr.contains(num1)){
                 num1v = true;    
             }
-            if(arr[i] == num2){
+            if(arr.contains(num2)){
                 num2v = true;
             }
         }
